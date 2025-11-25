109af754af7a349745e6e5bf296f6d9861e021c2
Rohan Parkes <rparkes@email.com>
2025-11-25 14:54:54 +1100

    Added Identity code.

---
+++ SportsStore/Models/ViewModels/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportsStore.Models.ViewModels
+{
+  public class LoginModel
+  {
+    [Required]
+    public required string Name { get; set; }
+
+    [Required]
+    public required string Password { get; set; }
+
+    public string ReturnUrl { get; set; } = "/";
+  }
+}
